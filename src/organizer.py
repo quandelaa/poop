4b94d144fecf63e19e260e387b388f64d6358339
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-02 23:03:47 +0700

    refactor: cleaned up the code for rewriting of mkrdirs()

--- src/organizer.py
+++ src/organizer.py
@@ -1,32 +1,13 @@
 from pathlib import Path
-from config import TERMS, EXT_COLORS 
+from config import EXT_COLORS 
 from rich.console import Console
 
 class Organizer:
-    def __init__(self, options: dict, path) -> None:
+    def __init__(self, options: dict, path, files: list) -> None:
         self.options = options.copy()
         self.console = Console()
         self.path = Path(path)
+        self.files = files.copy() 
 
     def mkdirs(self):
         paths = []
-
-        for opt in self.options:
-            if not self.options[opt]:
-                continue
-            
-            print(opt)
-            term = TERMS.get(opt)
-            color = EXT_COLORS.get(term)
-            name = self.console.input(f"name for the new [bold {color}]{term}[/] directory: ")
-
-            if name.isspace():
-                name = opt
-
-            path = Path(self.path.joinpath(name))
-            print(path)
-            # path.mkdir(exist_ok=True)
-
-            print(name)
-
-            paths.append(path)
