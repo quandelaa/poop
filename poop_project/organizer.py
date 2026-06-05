e918c7d97807c4c769ca3a453ed00c9926927e88
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-05 17:07:35 +0700

    feat: complete organize() function and completely refactor how print()s and input()s look with new color scheme/palette

--- poop_project/organizer.py
+++ poop_project/organizer.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from sys import exception
+from types import new_class
 from .config import COLORS, EXT_COLORS, EXTENSIONS 
 from rich.console import Console
 
@@ -13,22 +15,24 @@ class Organizer:
         self.dirs = dict()
         
         self.prompt_color = COLORS.get("prompt")
+        self.muted_color = COLORS.get("muted")
 
     def make_dirs(self):
         specs = self.options.get("spec")
         
         if specs is not None:
-            self.dirs["spec"] = self.path.joinpath(specs["name"])
+            self.dirs["specs"] = (self.path.joinpath(specs["name"]), specs["exts"])
             return
 
         name_choice = None
         if all(val is True for val in self.options.values()):
             while name_choice not in (False, True):
-                name_choice = self.console.input(f"name new directories after their own respective category name (y/N): ")
-                name_choice = True if name_choice == "y" else False if name_choice == "" else name_choice
+                name_choice = self.console.input(
+                    f"[{self.prompt_color}]>[/] "
+                    f"[{self.muted_color}]name directories after their category (y/N):[/] "
+                )
 
-        if name_choice is True:
-            print()
+                name_choice = True if name_choice == "y" else False if name_choice == "" or name_choice == "n" else name_choice
 
         for opt in self.options:
             val = self.options.get(opt)
@@ -36,8 +40,19 @@ class Organizer:
             if val is True:
                 color = EXT_COLORS.get(opt)
                 if not name_choice:
-                    name = self.console.input(f"name of the new directory to store [bold {color}]{opt.replace("_", " ")}[/] files: ").replace(" ", "_")
-                
+                    name = None
+                    has_same = self.path.joinpath(str(name)) in self.dirs.values()
+
+                    while has_same or name is None: 
+                        name = self.console.input(
+                            f"[{self.prompt_color}]>[/] "
+                            f"[{self.muted_color}]directory name for[/] "
+                            f"[bold {color}]{opt.replace('_', ' ')}[/] "
+                            f"[{self.muted_color}]files:[/] "
+                        ).replace(" ", "_")
+                    
+                        has_same = self.path.joinpath(name) in self.dirs.values()
+
                     if name.isspace() or name == "":
                         name = f"{opt}s"
                 else:
@@ -48,11 +63,35 @@ class Organizer:
                 self.dirs[opt] = self.path.joinpath(str(val))
 
     def organize(self):
+        new_files = self.files.copy()
+        specs = self.dirs.get("specs")
+
+        if specs is None:      
+            for file_type in self.dirs:
+                new_files.append([self.dirs[file_type], list()])
+        else:
+            new_files.append([self.dirs["specs"][0], list()])
+
         for file in self.files:
-            ext = Path(file).suffix
+            sfx = file.suffix.lower()
+            file_type = EXTENSIONS.get(sfx, "misc")
 
-            print(file)
-            print(EXTENSIONS.get(ext.lower(), "misc"))
-            
+            if specs is None:
+                raw_path = self.dirs.get(file_type)
+            else:
+                if sfx in specs[1]:
+                    raw_path = specs[0]
+                else:
+                    raw_path = None
 
+            if raw_path is None:
+                continue
 
+            for item in new_files:
+                if not isinstance(item, list) or not item[0] == raw_path:
+                    continue
+
+                item[1].append(file)
+                new_files.remove(file)
+
+        return new_files
